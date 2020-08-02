c59e10f082a5fd0b4b72a3afa861dc8a43c60371
Zachary Strangeway <zachary.strangeway@tasconline.com>
2020-08-02 01:02:17 -0500

    Updated footer

---
+++ web-app/components/shared/AppFooter.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer class="transparent">
+    <v-container>
+      <p>&copy; {{ new Date().getFullYear() }} Red Comet Creations, LLC</p>
+    </v-container>
+  </footer>
+</template>
+
+<script lang="ts">
+import Vue from "vue"
+
+export default Vue.extend({})
+</script>
+
+<style lang="scss" scoped></style>
