70d66470f52db0a1335242fcdf687bb037ac5532
Zachary Strangeway <zachary.strangeway@tasconline.com>
2020-09-16 01:01:59 -0500

    Implement get all contacts lambda

--- back-end/src/contact/getContacts.ts
+++ back-end/src/contact/getContacts.ts
@@ -1,8 +1,38 @@
-/* eslint-disable */
-import { APIGatewayProxyEvent, Context, APIGatewayProxyResult } from "aws-lambda"
-import { SuccessResponse } from "/opt/response/Response"
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda"
+import AWS from "aws-sdk"
+import { ErrorResponse, SuccessResponse } from "/opt/response/Response"
 
-exports.handler = async (event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> => {
-  // TODO: Implement
-  return new SuccessResponse("Hello World")
+const dynamodb = new AWS.DynamoDB({ apiVersion: "2012-08-10" })
+
+exports.handler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  const nextToken = event.headers.nextToken
+    ? { id: { S: `${event.headers.nextToken}` } }
+    : undefined
+  const params = {
+    TableName: `${process.env.CONTACT_TABLE}`,
+    Limit: 25,
+    ExclusiveStartKey: nextToken,
+  }
+
+  try {
+    const query = await dynamodb.scan(params).promise()
+    const result = {
+      contacts: query.Items?.map((item) => {
+        return {
+          id: item.id.S,
+          date: item.date.N,
+          email: item.email.S,
+          name: item.name.S,
+          subject: item.subject.S,
+          message: item.message.S,
+        }
+      }),
+      nextToken: query.LastEvaluatedKey?.id.S,
+    }
+    return new SuccessResponse(JSON.stringify(result))
+  } catch (err) {
+    return new ErrorResponse(503, "Unable to retrieve records at this time.")
+  }
 }
