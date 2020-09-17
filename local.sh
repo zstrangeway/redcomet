e617a48e2001719131b409293880111a36f33206
Zachary Strangeway <zachary.strangeway@tasconline.com>
2020-09-17 17:04:48 -0500

    fix sam local start-api

--- local.sh
+++ local.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 # Run serverless API locally on docker at http://127.0.0.1:5000
-# FIXME: This currently is not functining as SAM is unable to find the DynamoDB resrouce.
 sam local start-api \
   --profile default \
   --region us-east-1 \
   --parameter-overrides 'Environment=dev' \
   --port 5000
-  
